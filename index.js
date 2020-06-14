374288dc60e4a9df857d11d86bc9bef5ed7312e9
Kola <kolachana12@gmail.com>
2020-06-14 20:49:12 +0530

    fix ssl cert error

--- index.js
+++ index.js
@@ -11,8 +11,11 @@ SSL_KEY_FILE = './key.pem'
 SSL_CERT_FILE = './cert.pem'
 SSL_PASS = 'sampath'
 SERVER_PORT = process.env.PORT || 9000;
-SECURE = fs.existsSync(SSL_KEY_FILE)
+SECURE = false;
 
+if (fs.existsSync(SSL_KEY_FILE)) secure = true;
+
+console.log('Secure:', SECURE);
 var app = express();
 app.use(cors());
 app.set('views', path.join(__dirname, 'views'));
@@ -21,14 +24,13 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 
-var options = {
-    key: fs.readFileSync(SSL_KEY_FILE),
-    cert: fs.readFileSync(SSL_CERT_FILE),
-    passphrase: SSL_PASS
-};
-
 var server = null;
 if (SECURE) {
+    var options = {
+        key: fs.readFileSync(SSL_KEY_FILE),
+        cert: fs.readFileSync(SSL_CERT_FILE),
+        passphrase: SSL_PASS
+    };
     server = https.createServer(options, app).listen(SERVER_PORT, function(){
       console.log("secure server started on port - " + SERVER_PORT);
     });
