b4632dc6e6e0708a0d084061e43c95ad3609485c
Kola <kolachana12@gmail.com>
2020-06-14 16:09:51 +0530

    first working commit

---
+++ index.js
@@ -0,0 +1,46 @@
+var path = require('path');
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var express = require('express');
+var cors = require('cors');
+var routes = require('./routes');
+const { ExpressPeerServer } = require('peer');
+
+SSL_KEY_FILE = './key.pem'
+SSL_CERT_FILE = './cert.pem'
+SSL_PASS = 'sampath'
+SERVER_PORT = 443
+SECURE = fs.existsSync(SSL_KEY_FILE)
+
+var app = express();
+app.use(cors());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', routes);
+
+var options = {
+    key: fs.readFileSync(SSL_KEY_FILE),
+    cert: fs.readFileSync(SSL_CERT_FILE),
+    passphrase: SSL_PASS
+};
+
+var server = null;
+if (SECURE) {
+    server = https.createServer(options, app).listen(SERVER_PORT, function(){
+      console.log("secure server started on port - " + SERVER_PORT);
+    });
+} else {
+    server = http.createServer(options, app).listen(SERVER_PORT, function(){
+      console.log("server started on port - " + SERVER_PORT);
+    });
+}
+
+// signalling server
+const peerServer = ExpressPeerServer(server, {
+  path: '/'
+});
+
+app.use('/peerjs', peerServer);
